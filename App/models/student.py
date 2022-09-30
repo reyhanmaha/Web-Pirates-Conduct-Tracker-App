d745edcfee2b91cc8d72892a6041302b7ebe343d
amandalalman <alalman@yahoo.com>
2022-09-30 03:08:51 +0000

    tried incorp calcKarma into student model itself

--- App/models/student.py
+++ App/models/student.py
@@ -1,4 +1,5 @@
 from App.database import db
+from .review import *
 
 class student(db.Model):
     studentID = db.Column(db.Integer, primary_key=True)
@@ -6,12 +7,24 @@ class student(db.Model):
     lastName = db.Column(db.String(80), nullable=False)
     karmaScore = db.Column(db.Integer)
 
-    def __init__(self, studentID, firstName, lastName, karmaScore):
-        self.studentID = studentID
+    def __init__(self, firstName, lastName):
         self.firstName = firstName
         self.lastName = lastName
         self.karmaScore = 0
 
+    def toJSON(self):
+        return{
+            'studentID': self.studentID,
+            'firstName': self.firstName,
+            'lastName': self.lastName,
+            'karmaScore': self.karmaScore
+        }
 
-
-
+    def calculateKarmaScore(studentID):
+        total = 0
+        for value in review:
+            if review.studentID==studentID:
+                total = total + review.upvotes 
+                total = total - review.downvotes
+        karmaScore = total
+        return karmaScore
