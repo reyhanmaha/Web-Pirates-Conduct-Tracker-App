b49019fd89b80c1d0097010a1071e553a36deff0
amandalalman <alalman@yahoo.com>
2022-10-31 03:32:41 +0000

    just commented off duplicate fxn names in user

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -27,64 +27,64 @@ def get_all_users_json():
     users = [user.toJSON() for user in users]
     return users
 
-def create_review(studentID, id, details):
+# def create_review(studentID, id, details):
 
-    student = student.query(studentID)
+#     student = student.query(studentID)
 
-    if not student:
-        newStudent = student(firstName="update", lastName ="me")
-        db.session.add(newStudent)
-        db.session.commit() # not sure if needed
-        studentID = newStudent.studentID
+#     if not student:
+#         newStudent = student(firstName="update", lastName ="me")
+#         db.session.add(newStudent)
+#         db.session.commit() # not sure if needed
+#         studentID = newStudent.studentID
         
-    newreview = review(studentID, id, details)
-    db.session.add(newreview)
-    db.session.commit()
-    return newreview
+#     newreview = review(studentID, id, details)
+#     db.session.add(newreview)
+#     db.session.commit()
+#     return newreview
 
-#not sure if functions would require readded and commiting reviews to model
+# #not sure if functions would require readded and commiting reviews to model
 
-def rate_revew(reviewID, rate):
+# def rate_revew(reviewID, rate):
     
-    review = review.query(reviewID)
+#     review = review.query(reviewID)
 
-    if not review:
-        return("Review doesnt exist")
+#     if not review:
+#         return("Review doesnt exist")
 
-    if rate > 0:
-        review.upvotes += 1
-    else:
-        review.downvotes += 1
+#     if rate > 0:
+#         review.upvotes += 1
+#     else:
+#         review.downvotes += 1
 
-    return ("rated")
+#     return ("rated")
 
-def delete_review(reviewID):
-    data=review.query.get(reviewID)
-    if data==None:
-        return jsonify("Error, No review found with this ID")
-    db.session.delete(data)
-    db.session.commit()
-    return jsonify("Review has been deleted")
+# def delete_review(reviewID):
+#     data=review.query.get(reviewID)
+#     if data==None:
+#         return jsonify("Error, No review found with this ID")
+#     db.session.delete(data)
+#     db.session.commit()
+#     return jsonify("Review has been deleted")
 
-def edit_review(reviewID, details):
+# def edit_review(reviewID, details):
 
-    review = review.query.get(reviewID)
+#     review = review.query.get(reviewID)
 
-    if not review:
-        return("Review does not exist")
+#     if not review:
+#         return("Review does not exist")
     
-    review.details = details
-    db.session.add(review)
-    db.session.commit()
+#     review.details = details
+#     db.session.add(review)
+#     db.session.commit()
 
-    return("Review updated")
+#     return("Review updated")
 
-def view_review(reviewID):
+# def view_review(reviewID):
 
-    return(review.query(reviewID))
+#     return(review.query(reviewID))
 
-def view_all_reviews():
-    return(review.query.all())
+# def view_all_reviews():
+#     return(review.query.all())
 
 def update_user(id, username):
     user = get_user(id)
